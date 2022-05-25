9d371916eba6756af5a04364a0d361e222d3b59f
Sreshtha Dey <sreshthadey96@gmail.com>
2022-05-25 23:08:51 +0530

    changes

--- sitemap-generator.py
+++ sitemap-generator.py
@@ -1,22 +1,19 @@
 import datetime
-#from jinja2 import Template
-#import pandas as pd
 import xml.etree.cElementTree as ET
-#from lxml import etree
 import xml.dom.minidom
 import os
 import argparse
 import sys
 
-def generate_xml_template(root, url, lastModTime, changeFreq, priority):
-    
-    doc = ET.SubElement(root,"url")
-    ET.SubElement(doc,"loc").text = url
-    ET.SubElement(doc,"lastmod").text = lastModTime
-    ET.SubElement(doc,"changefreq").text = changeFreq
-    ET.SubElement(doc,"priority").text = priority
-    #xml_template = pretty_print_xml(doc) 
-    #return xml_template
+def read_file():
+    with open(args.input_file_path,'r') as file:
+        list_of_urls = file.read()
+        list_of_urls = list_of_urls.split('\n')
+    return list_of_urls
+
+def write_file(xml_template, XML_file_path):
+    with open(XML_file_path,'w') as file:
+        file.write(xml_template)
 
 def format_xml(root):
     
@@ -24,40 +21,42 @@ def format_xml(root):
     xml_string = os.linesep.join([s for s in xml_string.splitlines() if s.strip()])
     return xml_string
 
-def generate_xml_file(list_of_urls, XML_file_path):
+def generate_xml_template(root, url, last_mod_time, change_freq, priority):
+    
+    doc = ET.SubElement(root,"url")
+    ET.SubElement(doc,"loc").text = url
+    ET.SubElement(doc,"lastmod").text = last_mod_time
+    ET.SubElement(doc,"changefreq").text = change_freq
+    ET.SubElement(doc,"priority").text = priority
+
+def generate_xml_file(list_of_urls):
     
     root = ET.Element('urlset')
     root.attrib['xmlns:xsi'] = "http://www.w3.org/2001/XMLSchema-instance"
     root.attrib['xsi:schemaLocation'] = "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
     root.attrib['xmlns'] = "http://www.sitemaps.org/schemas/sitemap/0.9"
     
-    lastModTime = datetime.datetime.now().strftime('%Y-%m-%d')
-    changeFreq = 'daily' 
+    last_mod_time = datetime.datetime.now().strftime('%Y-%m-%d')
+    change_freq = 'daily' 
     priority = '1.0'
     
-    for url in list_of_urls:
-        url = args.baseURL + url
-        generate_xml_template(root, url, lastModTime, changeFreq, priority)
+    for i in range(len(list_of_urls)-1):
+        url = args.base_url + list_of_urls[i]
+        print(url)
+        generate_xml_template(root, url, last_mod_time, change_freq, priority)
         
     xml_template = format_xml(root) 
     tree = ET.ElementTree(root)
-    
-    print(xml_template)
-    
-    with open(XML_file_path,'w') as file:
-        file.write(xml_template)
+
+    return xml_template
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description = "List of arguments")
-    parser.add_argument ('baseURL', type = str, help= 'Base URL for this site')
-    parser.add_argument ('input_file_path', type = str, help= 'File path of text file for list of URLs')
-    parser.add_argument ('XML_file_path', type = str, help= 'File path of XML file')
+    parser.add_argument ('--base_url', type = str, default="", help= 'Base URL for this site')
+    parser.add_argument ('--input_file_path', type = str, required = True, help= 'File path of text file for list of URLs')
+    parser.add_argument ('--xml_file_path', type = str, required = True, help= 'File path of XML file')
     args = parser.parse_args()
-    print(args.baseURL)
     
-    with open(args.input_file_path,'r') as file:
-        list_of_urls = file.read()
-        list_of_urls = list_of_urls.split('\n')
-    
-    generate_xml_file(list_of_urls, args.XML_file_path)
+    xml_template = generate_xml_file(read_file())
+    write_file(xml_template, args.xml_file_path)
